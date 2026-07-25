506482661289b3f46242e3ec075f8eceb671688c
abhijithsai <abhijithsaithirunahari51@gmail.com>
2026-07-25 08:56:22 +0200

    [Workflow Serv] - Initiated the Workflow service

--- apps/organization/config/org_settings.py
+++ apps/organization/config/org_settings.py
@@ -2,12 +2,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     ORGANIZATION_DATABASE_URL: str
-    KEYCLOAK_URL: str
-    KEYCLOAK_REALM: str
-    KEYCLOAK_CLIENT_ID: str
-    KEYCLOAK_CLIENT_SECRET: str
-    KEYCLOAK_ADMIN_USER: str
-    KEYCLOAK_ADMIN_PASSWORD: str
     NATS_URL : str
 
     model_config = SettingsConfigDict(
