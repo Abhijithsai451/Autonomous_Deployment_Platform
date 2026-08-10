453d3f1f347dc9920834956e49cfa489ef6d5656
abhijithsai <abhijithsaithirunahari51@gmail.com>
2026-08-10 13:07:39 +0200

    [Messaging Platform] - created the events driven system for the identity service.

--- apps/identity/application/auth_service.py
+++ apps/identity/application/auth_service.py
@@ -1,12 +1,24 @@
+from uuid import UUID
+
 from sqlalchemy.orm import Session
 
+from apps.identity.domain.outbox import OutboxEvent, OutboxStatus
 from apps.identity.infrastructure.keycloak_client import KeycloakClient
-
 
 class AuthService:
     def __init__(self, db: Session, keycloak: KeycloakClient):
         self.db = db
         self.keycloak = keycloak
+
+    def _log_event(self, aggregate_id: UUID, aggregate_type: str,  event_type: str, payload:dict):
+        outbox_entry = OutboxEvent(
+                aggregate_id=aggregate_id,
+                aggregate_type=aggregate_type,
+                event_type = event_type,
+                payload= payload,
+            status = OutboxStatus.PENDING
+        )
+        self.db.add(outbox_entry)
 
     def handle_login(self, username, password):
         return self.keycloak.authenticate_user(username, password)
